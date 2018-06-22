e30ac15984c2abf9e490684d8fa02ffe1792adbf
Suraj Subbarao <suraj>
2018-06-22 12:24:29 +0530

    update

--- polly.py
+++ polly.py
@@ -6,8 +6,10 @@ arg = sys.argv[1:]
 string = ''.join(arg)
 
 polly = boto3.client('polly')
-text = polly.synthesize_speech(Text = string, OutputFormat = 'mp3', VoiceId = '')
+text = polly.synthesize_speech(Text = string, OutputFormat = 'mp3', VoiceId = 'Joanna')
 
 with open('output.mp3', 'wb') as file:
     file.write(text['AudioStream'].read())
     file.close()
+
+print("MP3 file created successfully.")
