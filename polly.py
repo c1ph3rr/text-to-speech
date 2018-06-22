4118db2e4cc5d7a2d25add4f26a725dea834e13e
Suraj Subbarao <suraj>
2018-06-22 11:40:48 +0530

    initial commit

---
+++ polly.py
@@ -0,0 +1,13 @@
+import boto3
+import os
+import sys
+
+arg = sys.argv[1:]
+string = ''.join(arg)
+
+polly = boto3.client('polly')
+text = polly.synthesize_speech(Text = string, OutputFormat = 'mp3', VoiceId = '')
+
+with open('output.mp3', 'wb') as file:
+    file.write(text['AudioStream'].read())
+    file.close()
